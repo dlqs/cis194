8cc60f4620a2464b0424485648dde36516500cc9
Donald Lee <dlqs@gmx.com>
2021-05-15 15:37:30 +0800

    update

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -6,12 +6,14 @@ import Log
 -- ex 1
 parseMessage :: String -> LogMessage
 
+-- "I"
 parseMessage s =
   let split = words s in
   case (head split) of
     "I" -> LogMessage Info (read $ split!!1) (unwords . drop 2 $ split)
     "W" -> LogMessage Warning (read $ split!!1) (unwords . drop 2 $ split)
     "E" -> LogMessage (Error (read $ split!!1)) (read $ split!!2) (unwords . drop 3 $ split)
+    _   -> Unknown s
 
 parse :: String -> [LogMessage]
 parse s = map parseMessage (lines s)
@@ -22,17 +24,26 @@ parse s = map parseMessage (lines s)
 insert :: LogMessage -> MessageTree -> MessageTree
 
 insert (Unknown _) mt = mt
-insert lm@(LogMessage _ _ _) Leaf = Node Leaf lm Leaf
+insert lm Leaf = Node Leaf lm Leaf
 insert lm@(LogMessage _ ts _) (Node lmt (nlm@(LogMessage _ nts _)) rmt) 
   | ts < nts = Node (insert lm lmt) nlm rmt
-  | ts > nts = Node lmt nlm (insert lm rmt)
+  | otherwise = Node lmt nlm rmt
+--  | ts >= nts = Node lmt nlm (insert lm rmt) (apparently >= is not exhaustive here)
+
+{- Shawn's
+insert :: LogMessage -> MessageTree -> MessageTree
+insert m Leaf = Node Leaf m Leaf
+insert m@(LogMessage _ ts _) (Node lt cm@(LogMessage _ cts _) rt)
+  | ts < cts = Node (insert m lt) cm rt
+  | otherwise = Node lt cm (insert m rt)
+insert (Unknown _) t = t
+insert _ _ = error "passed wrong constructor"
+ -}
 
 -- ex 3
 build :: [LogMessage] -> MessageTree
-build lms = helper lms Leaf
-  where
-    helper [] mt = mt
-    helper (x:xs) mt = helper xs $ insert x mt
+build [] = Leaf
+build (x:xs) = insert x $ build xs
 
 -- ex 4
 inOrder :: MessageTree -> [LogMessage]
@@ -46,7 +57,7 @@ whatWentWrong :: [LogMessage] -> [String]
 whatWentWrong lms =
   map messageString $ filter errorSeverityAtLeast50 $ inOrder $ build lms
   where
-    errorSeverityAtLeast50 lm@(LogMessage (Error e) _ _)
+    errorSeverityAtLeast50 (LogMessage (Error e) _ _)
       | e >= 50 = True
       | otherwise = False
     errorSeverityAtLeast50 _ = False
